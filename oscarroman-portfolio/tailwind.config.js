a98ed92b598954594bbfa9c9fe40f9dbc00842a8
Oscar Roman Quispe <oscarroman.pe@gmail.com>
2026-01-30 05:54:09 -0500

    background color

--- oscarroman-portfolio/tailwind.config.js
+++ oscarroman-portfolio/tailwind.config.js
@@ -1,4 +1,17 @@
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+  theme: {
+    extend: {
+      colors: {
+        'primary-dark': '#262a3b',
+        'card-dark': '#343848',
+        'accent-yellow': '#feef22',
+        'footer-dark': '#000000',
+      },
+      fontFamily: {
+        sans: ['"Nunito Sans"', 'system-ui', 'sans-serif'],
+      },
+    },
+  },
 }
